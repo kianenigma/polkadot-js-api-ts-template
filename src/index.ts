2bfb848cb0925a189ba9b94b6975cae92a664337
kianenigma <kian@parity.io>
2022-03-05 08:26:34 +0000

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
-// import { BN } from "@polkadot/util/bn/bn";
+import { Balance } from "@polkadot/types/interfaces/runtime";
+import "@polkadot/api-augment";
+import "@polkadot/types-augment";
 import yargs from 'yargs';
 import { hideBin } from "yargs/helpers"
 
@@ -17,6 +19,8 @@ async function main() {
 	const provider = new WsProvider(options.endpoint);
 	const api = await ApiPromise.create({ provider });
 	console.log(`Connected to node: ${options.endpoint} ${(await api.rpc.system.chain()).toHuman()} [ss58: ${api.registry.chainSS58}]`)
+	const ED: Balance = api.consts.balances.existentialDeposit;
+	console.log(ED.toHuman())
 }
 
 main().catch(console.error).finally(() => process.exit());
