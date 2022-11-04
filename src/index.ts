e38b3a71a4d9beff5a4b444974567bfd1fb1a883
kianenigma <kian@parity.io>
2022-11-04 13:36:56 +0100

    pools-metrics

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import '@polkadot/api-augment';
 import '@polkadot/types-augment';
 
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import { Balance } from '@polkadot/types/interfaces/runtime';
-import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { BN } from '@polkadot/util';
+import { GenericAccountId } from '@polkadot/types';
+import { BN, bnToU8a, stringToU8a, u8aConcat } from '@polkadot/util';
+import yargs from 'yargs';
 
 const optionsPromise = yargs(hideBin(process.argv)).option('endpoint', {
 	alias: 'e',
@@ -22,26 +22,97 @@ async function main() {
 	const api = await ApiPromise.create({ provider });
 
 	console.log(
-		`Connected to node: ${options.endpoint} ${(await api.rpc.system.chain()).toHuman()} [ss58: ${api.registry.chainSS58
+		`Connected to node: ${options.endpoint} ${(await api.rpc.system.chain()).toHuman()} [ss58: ${
+			api.registry.chainSS58
 		}]`
 	);
 
-
-	for (let key in api.query) {
-		console.log(key, api.query[key])
-		if (api.query[key]) {
-			console.log(await api.query[key].palletVersion());
-		}
+	function createAccount(
+		api: ApiPromise,
+		palletId: Uint8Array,
+		poolId: BN,
+		index: number
+	): GenericAccountId {
+		const EMPTY_H256 = new Uint8Array(32);
+		const MOD_PREFIX = stringToU8a('modl');
+		const U32_OPTS = { bitLength: 32, isLe: true };
+		return api.registry.createType(
+			'AccountId32',
+			u8aConcat(
+				MOD_PREFIX,
+				palletId,
+				new Uint8Array([index]),
+				bnToU8a(poolId, U32_OPTS),
+				EMPTY_H256
+			)
+		);
 	}
 
-	// reading a constant
-	const ED: Balance = api.consts.balances.existentialDeposit;
-	console.log(ED.toHuman());
+	const b = (x: unknown) => api.createType('Balance', x).toHuman();
 
-	// subscribe to finalized blocks:
-	const unsub = await api.rpc.chain.subscribeFinalizedHeads((header) => {
-		console.log(`finalized block #${header.number}`);
-	});
+	// count of all nomination pools.
+	const poolsCount = await api.query.nominationPools.counterForBondedPools();
+	// count of all members.
+	const membersCount = await api.query.nominationPools.counterForPoolMembers();
+
+	const PoolsDetails = await Promise.all(
+		(
+			await api.query.nominationPools.bondedPools.entries()
+		).map(async ([key, bondedPool]) => {
+			const poolId = key.args[0];
+			const members = bondedPool.unwrapOrDefault().memberCounter;
+			const totalPoints = bondedPool.unwrapOrDefault().points.toBn();
+			const palletId = api.consts.nominationPools.palletId.toU8a();
+			const rewardAccount = createAccount(api, palletId, poolId, 1);
+			const bondedAccount = createAccount(api, palletId, poolId, 0);
+			const existentialDeposit = api.consts.balances.existentialDeposit;
+			const pendingRewards = (await api.query.system.account(rewardAccount)).data.free.sub(
+				existentialDeposit
+			);
+			const ledger = (await api.query.staking.ledger(bondedAccount)).unwrap();
+			const totalBalance = ledger.total.toBn();
+			const ratioPercent = totalPoints.mul(new BN(100)).div(totalBalance);
+			const unbondingBalance = ledger.total.toBn().sub(ledger.total.toBn());
+			return {
+				// id of the pool.
+				poolId,
+				// number of members
+				members,
+				// total points in the pool.
+				totalPoints,
+				// total balance in the pool.
+				totalBalance,
+				// ratio of point to balance in this pool, in percent.
+				ratioPercent,
+				// amount of balance being unbonded form the pool
+				unbondingBalance,
+				// the total pending rewards of hte pool.
+				pendingRewards
+			};
+		})
+	);
+
+	console.log(`poolsCount: ${poolsCount}`);
+	console.log(`membersCount: ${membersCount}`);
+	console.log(
+		`average members per pool: ${(membersCount.toNumber() / poolsCount.toNumber()).toFixed(1)}`
+	);
+	console.log(
+		`sum(totalPoints): ${b(PoolsDetails.map((p) => p.totalPoints).reduce((p, c) => p.add(c)))}`
+	);
+	console.log(
+		`sum(totalBalance): ${b(PoolsDetails.map((p) => p.totalBalance).reduce((p, c) => p.add(c)))}`
+	);
+	console.log(
+		`sum(unbondingBalance): ${b(
+			PoolsDetails.map((p) => p.unbondingBalance).reduce((p, c) => p.add(c))
+		)}`
+	);
+	console.log(
+		`sum(pendingRewards): ${b(
+			PoolsDetails.map((p) => p.pendingRewards).reduce((p, c) => p.add(c))
+		)}`
+	);
 }
 
 main().catch(console.error);
